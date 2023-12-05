3b50c4d42b0ff6c1f2c8641c3a41fa8a828210da
JSONFN21 <152232516+JSONFN21@users.noreply.github.com>
2023-12-04 21:58:32 -0500

    Update goalkeeper_plots.R

--- goalkeeper_plots.R
+++ goalkeeper_plots.R
@@ -43,7 +43,7 @@ draw_plots <- function(x_var,y_var,x_title,y_title) {
   }
   
   
-  # Add Mean R-squared and R value annotated to plots 
+  #Mean R-squared and R value annotated to plots 
   plot_2 + annotate("text", x = max(x_var), y = max(y_var), 
                     label = paste("R-squared =", round(mean(r_squared_values), 3)),
                     hjust = 3.9, vjust = 3.5, color = "black")+
